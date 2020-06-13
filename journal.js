26cf2c401641f881e7920f765f62ccdde91e6595
Katiuscia Fanomezantsoa <kati.fan@onja.org>
2020-06-13 08:02:38 +0300

    adding the first step

--- journal.js
+++ journal.js
@@ -1,25 +1,14 @@
-// Greeting
-let welcoming = prompt('Welcome to my Onja journal!');
-let firstChoice = prompt('Choose (1) for all the entries.');
-let secondChoice = prompt('Choose(2) for adding a new entry.');
-let thirdChoice = prompt('Choose(3) to quit');
-
-console.log(greeting(firstChoice, secondChoice, thirdChoice)); 
-
-function listOfChoice(){
-  let choiceContent1 = ["The weather is good.", "The wonderful recipes"];
-  let choiceContent2 = ["It is sunny day.", "It is a beautiful day"];
-  let choiceContent3 = ["It is a beautiful day", "The weather is good."];
-    console.log(`${choiceContent1}, ${choiceContent2}, ${choiceContent3}`);
-
-  while(listOfChoice >= 1 && listOfChoice <= 3){
-    let numberOfContentChoices = prompt('Choose (1) for all the entries.');
-    if (numberOfContentChoices === 1){
-      console.log(listOfChoice);
-    }else if (secondChoice === 2){
-      listOfChoice.push("The weather is horrendous");
-    }else{
-     console.log("Error");
-    }
-  }
+// Create an array
+const listOfJournal = [
+  {
+    title: ["First blog letter", "Another day at Onja", "Today is a good day"],
+    content: ["The weather is good", "It is beautifull day.",  "Today is a good day"],
 }
+];
+
+let journal = listOfJournal;
+  if (journal === 1){
+    alert()
+  }
+
+const choiceContent = Number(prompt('Welcome to my Onja journal! \n Choose (1)for all the entries.\n  Choose (2) for adding a new entry \n  Choose (3) to quit'));
